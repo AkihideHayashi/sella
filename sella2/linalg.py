rename to sella2 and bug fix on rotation.__eq__

sella2/linalg.py
@@ -4,7 +4,7 @@ from typing import List
 from itertools import product
 import numpy as np
 
-from sella.hessian_update import update_H
+from sella2.hessian_update import update_H
 
 from scipy.sparse.linalg import LinearOperator
 from scipy.linalg import eigh

sella2/optimize/irc.py
@@ -3,7 +3,7 @@ import warnings
 import numpy as np
 from scipy.linalg import eigh
 
-from sella.peswrapper import PES
+from sella2.peswrapper import PES
 
 from ase.optimize.optimize import Optimizer
 from .restricted_step import IRCTrustRegion

setup.py
@@ -32,8 +32,8 @@ if debug:
 
 ext_modules = []
 for cy_file in cy_files:
-    ext_modules.append(Extension('.'.join(['sella', *cy_file]),
-                                 [os.path.join('sella', *cy_file) + cy_suff],
+    ext_modules.append(Extension('.'.join(['sella2', *cy_file]),
+                                 [os.path.join('sella2', *cy_file) + cy_suff],
                                  define_macros=macros,
                                  include_dirs=[np.get_include()]))
 
@@ -48,7 +48,7 @@ with open('README.md', 'r') as f:
 with open('requirements.txt', 'r') as f:
     install_requires = f.read().strip().split()
 
-setup(name='Sella',
+setup(name='Sella2',
       version=VERSION,
       author='Eric Hermes',
       author_email='ehermes@sandia.gov',